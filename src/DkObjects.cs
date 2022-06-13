d115df33ab8f778150e5aaf2e732a01d4805411f
DarkCompet <darkcompet@gmail.com>
2022-06-13 13:04:55 +0700

    Add doc

--- src/DkObjects.cs
+++ src/DkObjects.cs
@@ -3,6 +3,7 @@ namespace Tool.Compet.Core {
 
 	public class DkObjects {
 		/// Create new instance from given type.
+		/// Throws MissingMethodException if occured.
 		public static T NewInstace<T>() where T : class {
 			return (T)Activator.CreateInstance<T>();
 		}
