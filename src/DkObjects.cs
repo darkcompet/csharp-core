9af0a9a646ac03e9e00304028a73f4d10c08dde1
DarkCompet <darkcompet@gmail.com>
2022-07-13 01:03:39 +0700

    Cover

--- src/DkObjects.cs
+++ src/DkObjects.cs
@@ -6,13 +6,22 @@ namespace Tool.Compet.Core {
 		/// Create new instance from given type.
 		/// Throws exception if occured.
 		public static T NewInstace<T>() where T : class {
-			return (T)Activator.CreateInstance(
-				typeof(T),
-				BindingFlags.CreateInstance | BindingFlags.Public | BindingFlags.Instance | BindingFlags.OptionalParamBinding,
-				null,
-				new Object[] { Type.Missing },
-				null
-			)!;
+			try {
+				// Go with this option when no constructor is defined,
+				// or parameterless constructor exists.
+				return Activator.CreateInstance<T>();
+			}
+			catch {
+				// Go with this option when some constructor is defined,
+				// but this will ignore params.
+				return (T)Activator.CreateInstance(
+					typeof(T),
+					BindingFlags.CreateInstance | BindingFlags.Public | BindingFlags.Instance | BindingFlags.OptionalParamBinding,
+					null,
+					new Object[] { Type.Missing },
+					null
+				)!;
+			}
 		}
 	}
 }
