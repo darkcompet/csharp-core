853fcae0ebd383ecece6ef39999b37813d16e9ae
DarkCompet <darkcompet@gmail.com>
2022-08-01 08:38:42 +0700

    Change function to property for get method

--- src/DkUtils.cs
+++ src/DkUtils.cs
@@ -3,8 +3,6 @@ namespace Tool.Compet.Core {
 
 	public class DkUtils {
 		/// @return Number of milliseconds that have elapsed from 1970-01-01T00:00:00.000Z..
-		public static long CurrentUnixTimeInMillis() {
-			return DateTimeOffset.Now.ToUnixTimeMilliseconds();
-		}
+		public static long currentUnixTimeInMillis => DateTimeOffset.Now.ToUnixTimeMilliseconds();
 	}
 }
