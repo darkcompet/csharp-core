11e20a2f5d4140e63f8484cc0b4c73c83a536b8e
DarkCompet <darkcompet@gmail.com>
2022-09-28 17:52:24 +0700

    Add utcnow

--- src/DkUtils.cs
+++ src/DkUtils.cs
@@ -2,7 +2,10 @@ namespace Tool.Compet.Core {
 	using System;
 
 	public class DkUtils {
-		/// @return Number of milliseconds that have elapsed from 1970-01-01T00:00:00.000Z..
+		/// @return Number of milliseconds at now that have elapsed from 1970-01-01T00:00:00.000Z.
 		public static long currentUnixTimeInMillis => DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+		/// @return Number of milliseconds at utx-now that have elapsed from 1970-01-01T00:00:00.000Z.
+		public static long currentUtcUnixTimeInMillis => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 	}
 }
