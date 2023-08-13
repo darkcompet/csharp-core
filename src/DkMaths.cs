d15c3fa4e27cb7a159ce7ccaadaba8ff0b589c25
DarkCompet <darkcompet@gmail.com>
2023-08-13 14:01:48 +0700

    Add check digit

--- src/DkMaths.cs
+++ src/DkMaths.cs
@@ -1,4 +1,6 @@
 namespace Tool.Compet.Core {
+	using System;
+
 	public class DkMaths {
 		/// Fast pow with O(logN) time.
 		/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
@@ -32,10 +34,15 @@ namespace Tool.Compet.Core {
 		/// Calculate polynomial function: y(x) = c + x * (b + x * (a + x * 0))
 		public static long Poly(long[] arr, long x) {
 			var result = 0L;
-			for (int index = 0, N = arr.Length; index < N; ++index) {
+			var N = arr.Length;
+			for (var index = 0; index < N; ++index) {
 				result = arr[index] + x * result;
 			}
 			return result;
+		}
+
+		public static bool IsDigit(char ch) {
+			return '0' <= ch && ch <= '9';
 		}
 	}
 }
