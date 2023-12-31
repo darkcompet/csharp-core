9db2bdb9f2c090c7a9009b0627c16ac23634f047
DarkCompet <darkcompet@gmail.com>
2024-01-01 00:03:57 +0700

    Make math better

--- src/DkMaths.cs
+++ src/DkMaths.cs
@@ -1,41 +1,104 @@
 namespace Tool.Compet.Core;
 
 public class DkMaths {
-	/// Fast pow with O(logN) time.
+	/// <summary>
+	/// Fast pow.
+	/// Time: O(logN)
 	/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
 	/// Method 2: x^9 = x^4 * x^4 * x
-	public static long Pow(long x, int n) {
-		if (n < 0) {
-			var ans = Pow(x, -n);
-			return ans == 0 ? 0 : 1 / ans;
-		}
-		if (n == 0) {
-			return 1;
-		}
-		if (n == 1) {
-			return x;
-		}
-
+	/// </summary>
+	/// <param name="x">Base number</param>
+	/// <param name="n">Exponential number. It should not be negative</param>
+	/// <returns></returns>
+	public static long Pow(long x, uint n) {
 		var result = 1L;
 		while (n > 0) {
 			// Mul if meet bit 1
 			if ((n & 1) == 1) {
-				result = (result * x); // Mod here
+				result *= x;
 			}
 			// Down n and Up x
 			n >>= 1;
-			x = (x * x); // Mod here
+			x *= x;
 		}
 
 		return result;
 	}
 
-	/// Calculate polynomial function: y(x) = c + x * (b + x * (a + x * 0))
+	/// <summary>
+	/// Fast pow with modulo.
+	/// Time: O(logN)
+	/// Method 1: x^9 = x^0 * x^1 * x^0 * x^0 * x^8. Note: 9 = 1001.
+	/// Method 2: x^9 = x^4 * x^4 * x
+	/// </summary>
+	/// <param name="x">Base number</param>
+	/// <param name="n">Exponential number. It should not be negative</param>
+	/// <returns></returns>
+	public static long Pow(long x, uint n, int mod) {
+		var result = 1L;
+		while (n > 0) {
+			// Mul if meet bit 1
+			if ((n & 1) == 1) {
+				result = result * x % mod;
+			}
+			// Down n and Up x
+			n >>= 1;
+			x = x * x % mod;
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// For positive integer N, this finds largest number k such that: 2^k <= N.
+	/// Time: O(logN).
+	/// </summary>
+	/// <param name="n">Must be >= 1</param>
+	/// <returns></returns>
+	public static int FloorLog2(long n) {
+		var k = 0;
+		while ((1L << k) <= n) {
+			++k;
+		}
+		return k - 1;
+	}
+
+
+	/// <summary>
+	/// For any integer N, this finds lowest number k such that: 2^k >= N.
+	/// Time: O(logN).
+	/// </summary>
+	/// <param name="n">Should be >= 0</param>
+	/// <returns></returns>
+	public static int CeilLog2(long n) {
+		var k = 0;
+		while ((1L << k) < n) {
+			++k;
+		}
+		return k;
+	}
+
+	/// <summary>
+	/// For positive integer N, this checks given N is power of 2 or not.
+	/// Time: O(1).
+	/// </summary>
+	/// <param name="n">Any</param>
+	/// <returns></returns>
+	public static bool IsPowOf2(long n) {
+		return n > 0 && (n & (n - 1)) == 0;
+	}
+
+	/// <summary>
+	/// Calculate value of polynomial function. For eg,. f(x) = c + x * (b + x * (a + x * 0))
+	/// </summary>
+	/// <param name="arr">Store value of a_0, a_1, ..., a_n</param>
+	/// <param name="x">Base number</param>
+	/// <returns></returns>
 	public static long Poly(long[] arr, long x) {
 		var result = 0L;
 		var N = arr.Length;
 		for (var index = 0; index < N; ++index) {
-			result = arr[index] + x * result;
+			result = arr[index] + (x * result);
 		}
 		return result;
 	}
