bee2765d49a08a1e762e61d18fb725538753507a
DarkCompet <darkcompet@gmail.com>
2023-12-31 23:52:45 +0700

    Use new namespace declaration

--- src/DkBitwise.cs
+++ src/DkBitwise.cs
@@ -1,8 +1,13 @@
-namespace Tool.Compet.Core {
-	public class DkBitwise {
-		/// @param index: From right -> left. At rightmost, it is 0.
-		public static bool HasBitAt(int value, int index) {
-			return ((value >> index) & 1) == 1;
-		}
+namespace Tool.Compet.Core;
+
+public class DkBitwise {
+	/// <summary>
+	/// Check bit (1) at index of given number.
+	/// </summary>
+	/// <param name="value">The number</param>
+	/// <param name="index">From right -> left. At rightmost, it is 0.</param>
+	/// <returns></returns>
+	public static bool HasBitAt(int value, int index) {
+		return ((value >> index) & 1) == 1;
 	}
 }
--- src/DkKeyOrderedDictionary.cs
+++ src/DkKeyOrderedDictionary.cs
@@ -1,62 +1,62 @@
-namespace Tool.Compet.Core {
-	/// Default Dictionary will change (sort) keys, so it does not preserve order of keys.
-	/// This class keep tracking order of keys when add, remove, ... the entry.
-	public class DkKeyOrderedDictionary<T> {
-		private readonly List<string> _keys;
-		private readonly Dictionary<string, T> _map;
+namespace Tool.Compet.Core;
 
-		public DkKeyOrderedDictionary(int capacity = 10) {
-			this._keys = new(capacity);
-			this._map = new Dictionary<string, T>(capacity);
-		}
+/// Default Dictionary will change (sort) keys, so it does not preserve order of keys.
+/// This class keep tracking order of keys when add, remove, ... the entry.
+public class DkKeyOrderedDictionary<T> {
+	private readonly List<string> _keys;
+	private readonly Dictionary<string, T> _map;
 
-		/// Define the indexer to allow client code to use [] notation.
-		public T this[string key] {
-			get { return this._map[key]; }
-			set { this._map[key] = value; }
-		}
+	public DkKeyOrderedDictionary(int capacity = 10) {
+		this._keys = new(capacity);
+		this._map = new Dictionary<string, T>(capacity);
+	}
 
-		// public T this[int index] {
-		// 	get { var key = this.list[index]; return this.map[key]; }
-		// 	private set { }
-		// }
+	/// Define the indexer to allow client code to use [] notation.
+	public T this[string key] {
+		get { return this._map[key]; }
+		set { this._map[key] = value; }
+	}
 
-		public T? GetValueOrDefault(string key) {
-			return this._map.GetValueOrDefault(key);
-		}
+	// public T this[int index] {
+	// 	get { var key = this.list[index]; return this.map[key]; }
+	// 	private set { }
+	// }
 
-		public void Add(string key, T value) {
-			this._map.Add(key, value);
+	public T? GetValueOrDefault(string key) {
+		return this._map.GetValueOrDefault(key);
+	}
+
+	public void Add(string key, T value) {
+		this._map.Add(key, value);
+		this._keys.Add(key);
+	}
+
+	public bool TryAdd(string key, T value) {
+		if (this._map.TryAdd(key, value)) {
 			this._keys.Add(key);
+			return true;
 		}
+		return false;
+	}
 
-		public bool TryAdd(string key, T value) {
-			if (this._map.TryAdd(key, value)) {
-				this._keys.Add(key);
-				return true;
+	public int Count => this._keys.Count;
+	public long LongCount => this._keys.LongCount();
+
+	public string[] Keys {
+		get {
+			var arr = new string[this._keys.Count];
+			for (var index = this._keys.Count - 1; index >= 0; --index) {
+				arr[index] = this._keys[index];
 			}
-			return false;
+			return arr;
 		}
+		private set { }
+	}
 
-		public int Count => this._keys.Count;
-		public long LongCount => this._keys.LongCount();
+	public List<string> InternalKeys => this._keys;
 
-		public string[] Keys {
-			get {
-				var arr = new string[this._keys.Count];
-				for (var index = this._keys.Count - 1; index >= 0; --index) {
-					arr[index] = this._keys[index];
-				}
-				return arr;
-			}
-			private set { }
-		}
-
-		public List<string> InternalKeys => this._keys;
-
-		public void Clear() {
-			this._map.Clear();
-			this._keys.Clear();
-		}
+	public void Clear() {
+		this._map.Clear();
+		this._keys.Clear();
 	}
 }
