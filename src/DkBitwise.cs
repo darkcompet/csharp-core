58dfc031ebad4185e7a3cd31457d468fd311f8e3
DarkCompet <darkcompet@gmail.com>
2024-03-17 20:47:45 +0700

    Add, Clear, Toggle bit

--- src/DkBitwise.cs
+++ src/DkBitwise.cs
@@ -4,11 +4,11 @@ public class DkBitwise {
 	/// <summary>
 	/// Check bit (1) at index of given number.
 	/// </summary>
-	/// <param name="value">The number</param>
+	/// <param name="num">The number</param>
 	/// <param name="index">From right -> left. At rightmost, it is 0.</param>
 	/// <returns></returns>
-	public static bool HasBitAt(int value, int index) {
-		return ((value >> index) & 1) == 1;
+	public static bool HasBitAt(int num, int index) {
+		return ((num >> index) & 1) == 1;
 	}
 
 	/// <summary>
@@ -21,5 +21,32 @@ public class DkBitwise {
 	/// <returns></returns>
 	public static int BitCount(uint num) {
 		return System.Numerics.BitOperations.PopCount(num);
+	}
+
+	/// <summary>
+	/// Set bit (1) at given index.
+	/// </summary>
+	/// <param name="num">The number</param>
+	/// <param name="index">Rightmost index</param>
+	public static void SetBitAt(ref int num, int index) {
+		num |= 1 << index;
+	}
+
+	/// <summary>
+	/// Set bit (0) at given index.
+	/// </summary>
+	/// <param name="num">The number</param>
+	/// <param name="index">Rightmost index</param>
+	public static void ClearBitAt(ref int num, int index) {
+		num &= ~(1 << index);
+	}
+
+	/// <summary>
+	/// Set bit (0) at given index.
+	/// </summary>
+	/// <param name="num">The number</param>
+	/// <param name="index">Rightmost index</param>
+	public static void ToggleBitAt(ref int num, int index) {
+		num ^= 1 << index;
 	}
 }
